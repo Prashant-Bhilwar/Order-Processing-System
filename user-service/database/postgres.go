c629ab8686df5e338a7846ebe998415368595899
Prashant-Bhilwar <prashantbhilwar1@gmail.com>
2025-06-06 13:08:00 +0530

    feat: initialize user-service module with healthcheck and postgres connection

---
+++ user-service/database/postgres.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func InitPostgres() {
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		dbURL = "postgres://user:password@localhost:5432/orderdb?sslmode=disable"
+	}
+
+	var err error
+	DB, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Postgres connection error: %v", err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatalf("Postgres ping failed: %v", err)
+	}
+
+	fmt.Println("Postgres connected")
+}
