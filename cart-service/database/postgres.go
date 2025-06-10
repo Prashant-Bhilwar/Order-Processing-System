0cee259ba86a0fefa16ccb0f39199e0e2bfec991
Prashant-Bhilwar <prashantbhilwar1@gmail.com>
2025-06-10 19:27:34 +0530

    feat: connect PostgreSQL using GORM in cart-service

---
+++ cart-service/database/postgres.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/prashant-bhilwar/order-processing-system/cart-service/config"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitPostgres() {
+	var err error
+
+	dsn := config.DBUrl
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Postgres connection error: %v", err)
+	}
+
+	sqlDB, err := DB.DB()
+	if err != nil {
+		log.Fatalf("Failed to get SQL DB: %v", err)
+	}
+
+	err = sqlDB.Ping()
+	if err != nil {
+		log.Fatalf("Postgres ping failed: %v", err)
+	}
+
+	fmt.Println("PostgreSQL connected")
+}
--- cart-service/main.go
+++ cart-service/main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 
 	"github.com/prashant-bhilwar/order-processing-system/cart-service/config"
+	"github.com/prashant-bhilwar/order-processing-system/cart-service/database"
 )
 
 func main() {
 	config.LoadConfig()
 	fmt.Println("Cart service started")
+	database.InitPostgres()
 }
