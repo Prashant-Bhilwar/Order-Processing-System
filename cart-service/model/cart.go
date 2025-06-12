7f97794049acd712f68365c12ed43e483f2a2fab
Prashant-Bhilwar <prashantbhilwar1@gmail.com>
2025-06-12 16:25:50 +0530

    cart model fix

--- cart-service/model/cart.go
+++ cart-service/model/cart.go
@@ -2,7 +2,7 @@ package model
 
 type CartItem struct {
 	ID        uint `gorm:"primaryKey" json:"id"`
-	UserID    uint `json:"useer_id"`
+	UserID    uint `json:"user_id"`
 	ProductID uint `json:"product_id"`
 	Quantity  int  `json:"quantity"`
 }
